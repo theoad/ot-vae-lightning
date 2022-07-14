ccd271a0de45b102392b0af9df5b885c6c7e31a0
Theo <radraith@amazon.com>
2022-07-14 13:01:45 +0300

    updated requirements.txt to pass CI
    Signed-off-by: Theo <radraith@amazon.com>

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ot_vae_lightning',
     version='0.0.1',
     description='Implementation of Optimal Transport VAE',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Theo J. Adrai',
     author_email='tjtadrai@gmail.com',
     url='https://github.com/theoad/ot-vae-lightning',
-    install_requires=[
-        'torch',
-        'torchvision',
-        'torchaudio',
-        'pytorch-lightning',
-        'jsonargparse[signatures]',
-        'rich',
-        'torchmetrics',
-        'torch_fidelity',
-        'numpy',
-        'sympy'
-    ],
+    install_requires=requirements,
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
